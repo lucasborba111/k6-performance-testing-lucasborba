cc3d53f553cd1b5e4986db83d5930245d9298c5d
lucasborba111 <lucasborgesborba@gmail.com>
2024-11-27 20:34:10 -0300

    commit final

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,18 +4,32 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+const baseUrl = "https://rickandmortyapi.com/api";
+const getApi = () => ({
+  characters: `${baseUrl}/character`,
+  locations: `${baseUrl}/location`,
+  episodes: `${baseUrl}/episode`
+});
+
+export const getContactsDuration = new Trend('get_contacts', true);  
+export const RateContentOK = new Rate('content_OK');  
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_failed: ['rate<0.12'],  
+    get_contacts: ['p(95)<5700'],  
+    content_OK: ['rate>0.95']  
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
+    { duration: '30s', target: 5 },   
+    { duration: '30s', target: 5 },  
+    { duration: '30s', target: 5 },  
+    { duration: '30s', target: 5 },  
+    { duration: '30s', target: 10 },  
+    { duration: '30s', target: 50 },  
+    { duration: '30s', target: 50 },  
+    { duration: '30s', target: 80 },  
+    { duration: '1m', target:  80 } 
   ]
 };
 
@@ -27,8 +41,8 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
-
+  
+  const api = getApi();
   const params = {
     headers: {
       'Content-Type': 'application/json'
@@ -37,13 +51,16 @@ export default function () {
 
   const OK = 200;
 
-  const res = http.get(`${baseUrl}`, params);
+  const res = http.get(`${api.characters}`);  
 
+  
   getContactsDuration.add(res.timings.duration);
 
+  
   RateContentOK.add(res.status === OK);
 
+  
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'GET Episodes - Status 200': () => res.status === OK
   });
 }
